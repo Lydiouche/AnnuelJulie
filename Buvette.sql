da71c0a4985912493c9df866f187586fd145d51a
julie-bl <beloeil.julie@gmail.com>
2025-03-24 17:38:57 +0100

    Add files via upload

---
+++ Buvette.sql
@@ -0,0 +1,23 @@
+INSERT INTO Produit (NomP, Categorie, Quantite_Stock, Seuil_Alerte, Prix_Unitaire)
+VALUES 
+    ('Coca-Cola', 'Boisson', 100, 10, 2),
+    ('Sprite', 'Boisson', 100, 10, 2),
+    ('Fanta', 'Boisson', 100, 10, 2),
+    ('Oasis', 'Boisson', 100,10,2),
+    ('Biere Blonde', 'Alcool', 200,30,5),
+    ('Biere Ambree', 'Alcool', 150,20,5),
+    ('Vin blanc', 'Alcool', 150,20,5),
+    ('Shooter', 'Alcool', 200,30,3),
+    ('Eau Minerale', 'Boisson', 150, 20, 1),
+    ('Jus Orange', 'Boisson', 60, 10, 3),
+    ('Jus Abricot', 'Boissons', 60,10,3),
+    ('Perrier', 'Boisson', 60,10,3),
+    ('Sandwich Jambon', 'Snack', 200,20,4),
+    ('Sandwich Poulet', 'Snack', 200,20,4),
+    ('Salade Cesar', 'Snack', 100,10,4),
+    ('Barquettes frites', 'Snack', 150, 15,3),
+    ('Crudites sauce houmous', 'Snack', 100, 10,3),
+    ('6 Churros', 'Collation', 100, 10,6),
+    ('Doonuts', 'Collation', 100,10,4),
+    ('Cookies', 'Collation', 100,10,3);
+    
